d09e35cd2d5c70acec11dcb895b86e84ba304a5b
HULLINGB <bradley.hullinger@ucdenver.edu>
2022-08-04 21:49:30 -0600

    Create EncryptionAlgorithm.java

---
+++ EncryptionAlgorithm.java
@@ -0,0 +1,44 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Signature;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+
+public class EncryptionAlgorithm {
+   public static void main(String args[]) throws Exception{
+      //Creating a Signature object
+      Signature sign = Signature.getInstance("SHA256withRSA");
+      
+      //Creating KeyPair generator object
+      KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+      
+      //Initializing the key pair generator
+      keyPairGen.initialize(2048);
+      
+      //Generating the pair of keys
+      KeyPair pair = keyPairGen.generateKeyPair();      
+	
+      //Creating a Cipher object
+      Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        
+      //Initializing a Cipher object
+      cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+	  
+      //Adding data to the cipher
+      byte[] input = "Welcome to Tutorialspoint".getBytes();	  
+      cipher.update(input);
+	  
+      //encrypting the data
+      byte[] cipherText = cipher.doFinal();	 
+      System.out.println(new String(cipherText, "UTF8"));
+	  
+	  /**
+	  Encrypted Text:
+"???:]J_?]???;Xl??????*@??u???r??=T&???_?_??.??i?????(?$_f?zD??????ZGH??g???
+g?E:_??bz^??f?~o???t?}??u=uzp\UI????Z??l[?G?3??Y?UAEfKT?f?O??N_?d__?????a_?15%?^?
+'p?_?$,9"{??^??y??_?t???,?W?PCW??~??[?$??????e????f?Y-Zi__??_??w?_?&QT??`?`~?[?K_??_???
+	  **/
+   }
+}
+
